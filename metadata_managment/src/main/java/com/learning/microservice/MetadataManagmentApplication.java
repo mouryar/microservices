9f1746771d1243921aa2530906b92110222151cc
Mourya Rajala <mrajala@SEAC02R99C2G8WN.sea.corp.expecn.com>
2017-06-10 11:53:52 -0700

    Adding metadata_managment micro service

---
+++ metadata_managment/src/main/java/com/learning/microservice/MetadataManagmentApplication.java
@@ -0,0 +1,11 @@
+package com.learning.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetadataManagmentApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MetadataManagmentApplication.class, args);
+	}
+}
