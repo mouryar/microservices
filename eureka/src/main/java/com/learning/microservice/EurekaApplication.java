c6f60b3db885bb2b00c181aa2507bd3f7a07dd82
mouryar <mourya.rajala@gmail.com>
2017-06-11 03:23:17 -0700

    Adding service discovery

--- eureka/src/main/java/com/learning/microservice/EurekaApplication.java
+++ eureka/src/main/java/com/learning/microservice/EurekaApplication.java
@@ -2,12 +2,13 @@ package com.learning.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-@EnableDiscoveryClient
-public class MetadataManagmentApplication {
+public class EurekaApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(MetadataManagmentApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 }
